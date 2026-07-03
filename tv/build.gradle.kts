676134e6e28d75a6cc5c4866ce52c24a5e2874cb
Lau1907 <130590443+Lau1907@users.noreply.github.com>
2026-07-02 22:52:23 -0600

    Creacion del proyecto con modulos app, wear y tv

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.gms.google-services")
+    //id("com.google.gms.google-services")
 }
 
 android {
@@ -38,8 +38,8 @@ dependencies {
     implementation("androidx.tv:tv-material:1.0.0-alpha10")
 
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
-    implementation("com.google.firebase:firebase-database-ktx")
+    //implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    //implementation("com.google.firebase:firebase-database-ktx")
 
     // Imágenes
     implementation("io.coil-kt:coil-compose:2.7.0")
